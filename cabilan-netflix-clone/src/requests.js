fa20ab5e7397f3b38bed24541c5d28e0ff45a641
CKLuna17 <chriskate.luna@gmail.com>
2026-04-07 16:14:28 +0800

    adjust banner

--- cabilan-netflix-clone/src/requests.js
+++ cabilan-netflix-clone/src/requests.js
@@ -1,4 +1,4 @@
-const API_KEY = "12af6d37d67b93dc3f9b822039b0a434";
+const API_KEY = process.env.REACT_APP_API_KEY
 
 const requests = {
   fetchNetflixOriginals: `/discover/tv?api_key=${API_KEY}&with_networks=213`,
