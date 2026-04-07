cea887345cff9708a7558f40c3ccb5d41607aa6b
escultura-ama <jerome.escultura@ama.edu.ph>
2026-04-07 17:34:52 +0800

    fix the package json issue

--- src/Row.js
+++ src/Row.js
@@ -1,14 +1,39 @@
-import React, { useState, useEffect } from 'react'
-import axios from './axios';
-import './Row.css';
-import YouTube from 'react-youtube';
-import movieTrailer from 'movie-trailer';
+import React, { useState, useEffect, useRef } from "react"; // Added useRef
+import axios from "./axios";
+import "./Row.css";
+import YouTube from "react-youtube";
+import movieTrailer from "movie-trailer";
 
-const base_url = "https://image.tmdb.org/t/p/original/";
+const base_url = "https://image.tmdb.org/t/p/";
 
 function Row({ title, fetchUrl, isLargeRow }) {
   const [movies, setMovies] = useState([]);
   const [trailerUrl, setTrailerUrl] = useState("");
+
+
+  const rowRef = useRef(null);
+  const [isScrolling, setIsScrolling] = useState(false);
+  const [startX, setStartX] = useState(0);
+  const [scrollLeft, setScrollLeft] = useState(0);
+
+  const onMouseDown = (e) => {
+    setIsScrolling(true);
+
+    setStartX(e.pageX - rowRef.current.offsetLeft);
+    setScrollLeft(rowRef.current.scrollLeft);
+  };
+
+  const stopScrolling = () => {
+    setIsScrolling(false);
+  };
+
+  const onMouseMove = (e) => {
+    if (!isScrolling) return;
+    e.preventDefault();
+    const x = e.pageX - rowRef.current.offsetLeft;
+    const walk = (x - startX) * 0.7;
+    rowRef.current.scrollLeft = scrollLeft - walk;
+  };
 
   useEffect(() => {
     async function fetchData() {
@@ -16,51 +41,68 @@ function Row({ title, fetchUrl, isLargeRow }) {
       setMovies(request.data.results);
       return request;
     }
-
     fetchData();
   }, [fetchUrl]);
 
   const opts = {
-    height: '390',
-    width: '100%',
+    height: "390",
+    width: "100%",
     playerVars: {
-      // https://developers.google.com/youtube/player_parameters
       autoplay: 1,
     },
   };
 
-  const handleClick = (movie) => {
+  const handleclick = (movie) => {
     if (trailerUrl) {
-      setTrailerUrl('');
+      setTrailerUrl("");
     } else {
-      movieTrailer(movie?.name || movie?.title || "")
-      .then((url) => {
-        const urlParams = new URLSearchParams(new URL(url).search);
-        setTrailerUrl(urlParams.get('v'));
-      })
-      .catch((error) => console.log(error));
+      movieTrailer(movie?.name || movie?.title || movie?.original_name || "")
+        .then((url) => {
+          const urlParams = new URLSearchParams(new URL(url).search);
+          setTrailerUrl(urlParams.get("v"));
+        })
+        .catch((error) => console.log("Trailer Error:", error));
     }
   };
 
   return (
     <div className="row">
       <h2>{title}</h2>
-        
-        <div className="row__posters">
-        {movies.map(movie => (
-            <img
+      <div
+        className="row__posters"
+        ref={rowRef}
+        onMouseDown={onMouseDown}
+        onMouseMove={onMouseMove}
+        onMouseUp={stopScrolling}
+        onMouseLeave={stopScrolling}
+        style={{
+          cursor: isScrolling ? "grabbing" : "grab"
+        }}
+      >
+        {movies && movies.map((movie) => (
+          <img
             key={movie.id}
-            onClick={() => handleClick(movie)}
-            className={`row__poster ${isLargeRow && "row__posterLarge"}`}
-            src={`${base_url}${
-              isLargeRow ? movie.poster_path : movie.backdrop_path}`}
-              alt={movie.name}
-              />
-          ))}
-        </div>
-        {trailerUrl && <YouTube videoId={trailerUrl} opts={opts} />}
+            onClick={() => handleclick(movie)}
+            className={`row__poster ${isLargeRow ? "row__posterLarge" : ""}`}
+            src={
+              isLargeRow
+                ? movie.poster_path
+                  ? `${base_url}w500${movie.poster_path}`
+                  : "/no-image.png"
+                : movie.backdrop_path
+                  ? `${base_url}w780${movie.backdrop_path}`
+                  : "/no-image.png"
+            }
+            alt={movie.name || movie.title}
+            loading="lazy"
+            draggable="false"
+          />
+        ))}
+      </div>
+
+      {trailerUrl && <YouTube videoId={trailerUrl} opts={opts} />}
     </div>
-  )
+  );
 }
 
-export default Row
+export default Row;
